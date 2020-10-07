302ed9a7d5065793c394a053bbbea49a6bcc1d49
Moo4Cash <67906997+Moo4Cash@users.noreply.github.com>
2020-10-07 22:13:14 +0200

    klar

--- a_star.py
+++ a_star.py
@@ -1,76 +1,86 @@
 """Module for utelizing A* on a 2D grid"""
 
+# Birk Brunell DVADS20h
+# Estimerad tid 6h
+# Faktisk tid 8h
+
+
 import math
 from grid import Occupation
 from collections import defaultdict
 
 def reconstruct_path(came_from, current):
+    """Reconstructs the shortest path (there can be multiple paths of the same distance but the algorithm stops after finding 1)"""
     total_path = [current]
-    while current in came_from.Keys:
+    while current in came_from.keys():
         current = came_from[current]
         total_path.insert(0, current)
+    print("Total Path: ", total_path)
     return total_path
 
 
-def h(start):
-
-	return
-
-
-def lowest_F_in_open_set(start):
-	
-	return
+def h_score(start, goal):
+    """Calculates shortest possible theoretical path from given point (start) to the end (goal) using manhattan distance"""
+    h_score = abs(start[0] - goal[0]) + abs(start[1] - goal[1])
+    return h_score
 
 
-def neighbor(current):
+def neighbors(current, grid):
+    """Finds all four grid neighbors and checks if they are outside the grid or if they are BLOCKED"""
+    # fixed grid size of 15
+    grid_max = 14
+    true_neighbors = []
+    potential_neighbors = []
 
-	return
+    # Finds all potential neighbors
+    potential_neighbors.append(tuple([current[0] + 1, current[1]]))
+    potential_neighbors.append(tuple([current[0], current[1] + 1]))
+    potential_neighbors.append(tuple([current[0] - 1, current[1]]))
+    potential_neighbors.append(tuple([current[0], current[1] - 1]))
+    print(potential_neighbors)
+
+    # Checks if they are allowed
+    for potential_neighbor in potential_neighbors:
+        if potential_neighbor[0] >= 0 and potential_neighbor[0] <= grid_max and potential_neighbor[1] >= 0 and potential_neighbor[1] <= grid_max:
+            if grid[potential_neighbor[0]][potential_neighbor[1]] != Occupation.BLOCKED:
+                true_neighbors.append(potential_neighbor)
+    return true_neighbors
+
 
 def a_star(grid, start, goal):
-	"""Function to calculate a path through a grid"""
-	# The set of discovered nodes that may need to be (re-)expanded.
-    # Initially, only the start node is known.
-	open_set = [start]
-
-	# For node n, came_from[n] is the node immediately preceding it on the cheapest path from start
-    # to n currently known.
+    """Function to calculate a path through a grid"""
+    open_set = [start]
     came_from = {}
-
-	# For node n, g_score[n] is the cost of the cheapest path from start to n currently known.
+    
     g_score = defaultdict(lambda: math.inf)
-    g_score[start] = 0      
+    g_score[start] = 0    
 
-	# For node n, f_score[n] = g_score[n] + h(n). f_score[n] represents our current best guess as to
-    # how short a path from start to finish can be if it goes through n.
     f_score = defaultdict(lambda: math.inf)
-    f_score[start] = h(start)
-
-    while open_set
-        # This operation can occur in O(1) time if open_set is a min-heap or a priority queue
-        current = lowest_F_in_open_set():
-        if current = goal
+    f_score[start] = h_score(start, goal)
+    
+    # Runs as long as there exists unexplored paths
+    while open_set:
+        current = math.inf
+        # Finds node with lowest f_score in open_set
+        for node in open_set:
+            f_score[node] = g_score[node] + h_score(node, goal)
+            if f_score[node] < f_score[current]:
+                current = node
+    
+        # Checks if goal has been reached
+        if current == goal:
             return reconstruct_path(came_from, current)
 
-        open_set.Remove(current)
-        for neighbor(current)
-            # d(current,neighbor) is the weight of the edge from current to neighbor
-            # tentative_g_score is the distance from start to the neighbor through current
+        open_set.remove(current)
+        # Checks if new path to neighbors has lower g_score
+        for neighbor in neighbors(current, grid):
             tentative_g_score = g_score[current] + 1
-            if tentative_g_score < g_score[neighbor]
-                # This path to neighbor is better than any previous one. Record it!
+            if tentative_g_score < g_score[neighbor]:
                 came_from[neighbor] = current
                 g_score[neighbor] = tentative_g_score
-                f_score[neighbor] = g_score[neighbor] + h(neighbor)
-                if neighbor not in open_set
-                    open_set.add(neighbor)
+                f_score[neighbor] = g_score[neighbor] + h_score(neighbor, goal)
+                if neighbor not in open_set:
+                    open_set.append(neighbor)
 
     # Open set is empty but goal was never reached
     return None
-
-
-def main():
-
-	return
-
-
-main()
